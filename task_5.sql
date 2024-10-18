fe61545f1e440f57c8a2655fb39b13581324f93b
obum <obumanichebe@gmail.com>
2024-10-18 10:15:53 +0100

    Update: Added the table_row_insertion_code

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 INSERT INTO alx_book_store.Customers
 (customer_id, customer_name, email, address)
 VALUES
-(1, 'Anichebe Obumneme', 'aniobum@gmail.com', '12 Golden Cresent Estate, New York, USA ')
+(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
