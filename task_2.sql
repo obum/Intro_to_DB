e20032cae5c9e3c65bb475fcdeb87e183c855028
obum <obumanichebe@gmail.com>
2024-10-18 09:06:35 +0100

    Update: Added the table_creation_code

---
+++ task_2.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS Customers (
+customer_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Books(
+book_id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(130),
+author_id INT,
+price DOUBLE,
+publication_date DATE,
+FOREIGN KEY (author_id) references Authors(author_id)
+);
+
+CREATE TABLE IF NOT EXISTS Authors(
+author_id INT AUTO_INCREMENT PRIMARY KEY,
+author_name VARCHAR(215)
+);
+
+CREATE TABLE IF NOT EXISTS Orders(
+order_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_id INT,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+order_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS Order_Details(
+order_detail_id INT PRIMARY KEY,
+order_id INT,
+book_id INT,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id),
+quantity DOUBLE
+);
