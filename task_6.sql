679376d3ca275e4732787e035e4ec8af2b414ed4
obum <obumanichebe@gmail.com>
2024-10-18 10:25:59 +0100

    Update: Added the muliple_insertion_code

---
+++ task_6.sql
@@ -0,0 +1,10 @@
+USE alx_book_store;
+
+INSERT INTO customer
+(customer_id, customer_name, email, address)
+VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.comm', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamol', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
+
+SELECT * FROM alx_book_store.customer;
